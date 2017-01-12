ba61a5e3806a63af32ba04942e9728dbaf719f24
Serhii <serhii.sakhniuk>
2017-01-13 00:17:01 +0200

    Add some pure

--- chapter5.js
+++ chapter5.js
@@ -3,20 +3,34 @@ let _ = require("Ramda")
 
 let compose = (a, b) => (n) => a(b(n));
 
+let toUpperCase = str => str.toUpperCase();
+
+let exclaim = str => str + '!';
+
+let shout = compose(toUpperCase, exclaim);
+
 let head = a => a[0];
 
 let reverse = _.reduce((acc, x) => [x].concat(acc), []);
 
-console.log(reverse([1, 2, 3, 4]))
-
 let reverse2 = (ary) => {
+    let newArray = [].concat(ary);
     for (let i = 0; i < ary.length / 2; i++) {
-        [ary[i], ary[ary.length - 1 - i]] = [ary[ary.length - 1 - i], ary[i]];
+        [newArray[i], newArray[newArray.length - 1 - i]] = [newArray[newArray.length - 1 - i], newArray[i]];
     }
-    return ary;
+    return newArray;
 }
 
-let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let b = reverse2(a);
-console.log(a)
+
+let words = ['jumpkick', 'roundhouse', 'uppercut'];
+let loudLast = compose(shout, compose(head, reverse2));
+let loudLast2 = compose(compose(shout, head), reverse2);
+
+
+console.log(loudLast(words));
+console.log(loudLast2(words));
+
+
+
+
 
